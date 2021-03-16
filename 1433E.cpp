a79c2fdda76fcdf1e8b215fbbcb6ad81eb397925
piyushdubey-pd <piyushdubey72090@gmail.com>
2021-03-17 00:18:56 +0530

    logic modified

--- 1433E.cpp
+++ 1433E.cpp
@@ -7,7 +7,10 @@ int main(){
     cin>>n;
     for(int i=2;i<n;i++)
     p*=i;
+    // best self explainable anwer is..
+    // (n-1)P(n/2) * ((n/2)-1)
     cout<<p/(n/2)<<endl;
+
     // n! is for linear arrangement circular is (n-1)! that is nothing but n!/n
     //  so in this case we have covered circular of first then dividing the other
 }
